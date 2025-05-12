105a687517939d66d051e2323eeb57d09c0b275d
vkw28 <120770007+vzkw28@users.noreply.github.com>
2025-05-12 20:36:16 +0700

    Step 3: Add form to input new books and display them

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
 const myLibrary = [];
 
-function Book(title,author,pages,isRead) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+function Book(title, author, pages, isRead) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
-function addBookToLibrary(title,author,pages,isRead) {
-    const newBook = new Book(title,author,pages,isRead);
-    myLibrary.push(newBook);
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+  displayBooks();
 }
 
 function displayBooks() {
-    const container = document.getElementById('books-container');
-    container.innerHTML = '';
+  const container = document.getElementById("books-container");
+  container.innerHTML = "";
 
-    myLibrary.forEach(book => {
-        const card = document.createElement('div');
-        card.classList.add('book-card');
-        card.setAttribute('data-id', book.id);
+  myLibrary.forEach((book) => {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+    card.setAttribute("data-id", book.id);
 
-        card.innerHTML = `
+    card.innerHTML = `
         <h3>${book.title}</h3>
         <p><strong>Author:<strong> ${book.author}</p>
         <p><strong>Pages:<strong> ${book.pages}</p>
-        <p><strong>Read:<strong> ${book.isRead ? 'Yes' : 'No'}</p>
+        <p><strong>Read:<strong> ${book.isRead ? "Yes" : "No"}</p>
         `;
 
-        container.appendChild(card);
-    });
+    container.appendChild(card);
+  });
 }
 
-addBookToLibrary("1984", "George Orwell", 328, true);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("Clean Code", "Robert C. Martin", 464, true);
+const form = document.getElementById("book-form");
+const newBookBtn = document.getElementById("new-book-btn");
 
-displayBooks();
+newBookBtn.addEventListener("click", () => {
+  form.style.display = form.style.display === "none" ? "block" : "none";
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = parseInt(document.getElementById("pages").value);
+  const isRead = document.getElementById("isRead").checked;
+
+  addBookToLibrary(title,author,pages,isRead);
+
+  form.reset();
+  form.style.display = 'none';
+});
